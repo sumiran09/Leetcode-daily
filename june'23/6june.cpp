16312096e10b2dec13967f81f3dee931fea37d56
sumiran09 <112773734+sumiran09@users.noreply.github.com>
2023-06-06 18:55:28 +0530

    6june.cpp

---
+++ june'23/6june.cpp
@@ -0,0 +1,17 @@
+/*ques: Can Make Arithmetic Progression From Sequence
+A sequence of numbers is called an arithmetic progression if the difference between any two consecutive elements is the same.
+Given an array of numbers arr, return true if the array can be rearranged to form an arithmetic progression. Otherwise, return false.*/
+
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int d=arr[1]-arr[0];
+        for(int i=2;i<arr.size();i++){
+            if(arr[i]-arr[i-1]!=d){
+                return false;
+            }
+        }
+        return true;
+    }
+};
